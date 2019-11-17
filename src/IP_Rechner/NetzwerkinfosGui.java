5c81a209988cc316cbf60a6c8426fc73a2512aa0
Christoph <christoph@christoph-pircher.de>
2019-11-17 20:24:39 +0100

    MainGui und NetzerkinfosGui angepasst

--- src/IP_Rechner/NetzwerkinfosGui.java
+++ src/IP_Rechner/NetzwerkinfosGui.java
@@ -39,91 +39,91 @@ public class NetzwerkinfosGui {
 	 */
 	protected void createContents() {
 		shlNetzwerkinformationenBerechnen = new Shell();
-		shlNetzwerkinformationenBerechnen.setSize(436, 238);
+		shlNetzwerkinformationenBerechnen.setSize(485, 282);
 		shlNetzwerkinformationenBerechnen.setText("Netzwerkinformationen berechnen");
 		
 		Label lblGebenSieEine = new Label(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		lblGebenSieEine.setBounds(10, 10, 414, 15);
+		lblGebenSieEine.setBounds(10, 10, 414, 21);
 		lblGebenSieEine.setText("Geben Sie eine IP-Adresse und einen Prefix ein:");
 		
 		Label lblIpadresse = new Label(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		lblIpadresse.setBounds(20, 34, 64, 15);
+		lblIpadresse.setBounds(20, 37, 75, 21);
 		lblIpadresse.setText("IP-Adresse:");
 		
 		Spinner okt1 = new Spinner(shlNetzwerkinformationenBerechnen, SWT.BORDER);
 		okt1.setMaximum(255);
-		okt1.setBounds(90, 31, 47, 22);
+		okt1.setBounds(101, 37, 55, 24);
 		
 		Label label = new Label(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		label.setBounds(138, 38, 5, 15);
+		label.setBounds(158, 39, 4, 21);
 		label.setText(".");
 		
 		Spinner okt2 = new Spinner(shlNetzwerkinformationenBerechnen, SWT.BORDER);
 		okt2.setMaximum(255);
-		okt2.setBounds(143, 31, 47, 22);
+		okt2.setBounds(162, 37, 55, 24);
 		
 		Label label_1 = new Label(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		label_1.setBounds(191, 38, 5, 15);
+		label_1.setBounds(218, 39, 4, 21);
 		label_1.setText(".");
 		
 		Spinner okt3 = new Spinner(shlNetzwerkinformationenBerechnen, SWT.BORDER);
 		okt3.setMaximum(255);
-		okt3.setBounds(196, 31, 47, 22);
+		okt3.setBounds(223, 37, 55, 24);
 		
 		Label label_2 = new Label(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		label_2.setBounds(244, 38, 5, 15);
+		label_2.setBounds(280, 39, 4, 21);
 		label_2.setText(".");
 		
 		Spinner okt4 = new Spinner(shlNetzwerkinformationenBerechnen, SWT.BORDER);
 		okt4.setMaximum(255);
-		okt4.setBounds(249, 31, 47, 22);
+		okt4.setBounds(284, 37, 55, 24);
 		
 		Label lblPrefix = new Label(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		lblPrefix.setBounds(307, 34, 38, 15);
+		lblPrefix.setBounds(350, 37, 40, 21);
 		lblPrefix.setText("Prefix:");
 		
 		Spinner prefix = new Spinner(shlNetzwerkinformationenBerechnen, SWT.BORDER);
 		prefix.setMaximum(31);
-		prefix.setBounds(351, 31, 47, 22);
+		prefix.setBounds(396, 37, 47, 24);
 		
 		Button btnNewButton = new Button(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		btnNewButton.setBounds(30, 59, 358, 25);
+		btnNewButton.setBounds(30, 67, 403, 25);
 		btnNewButton.setText("Berechne Netzwerkinformationen");
 		
 		Label lblIpadresse_1 = new Label(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		lblIpadresse_1.setBounds(20, 90, 59, 15);
+		lblIpadresse_1.setBounds(20, 98, 75, 21);
 		lblIpadresse_1.setText("IP-Adresse:");
 		
 		Label lblNetzwerkadresse = new Label(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		lblNetzwerkadresse.setBounds(20, 111, 91, 15);
+		lblNetzwerkadresse.setBounds(20, 125, 117, 21);
 		lblNetzwerkadresse.setText("Netzwerkadresse:");
 		
 		Label lblSubnetzmaskePrefix = new Label(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		lblSubnetzmaskePrefix.setBounds(20, 132, 115, 15);
+		lblSubnetzmaskePrefix.setBounds(20, 152, 149, 21);
 		lblSubnetzmaskePrefix.setText("Subnetzmaske, Prefix:");
 		
 		Label lblKleinsteHostadresse = new Label(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		lblKleinsteHostadresse.setBounds(20, 153, 118, 15);
+		lblKleinsteHostadresse.setBounds(20, 179, 150, 21);
 		lblKleinsteHostadresse.setText("Kleinste Host-Adresse:");
 		
 		Label lblGrteHostadresse = new Label(shlNetzwerkinformationenBerechnen, SWT.NONE);
-		lblGrteHostadresse.setBounds(20, 174, 117, 15);
+		lblGrteHostadresse.setBounds(20, 206, 143, 21);
 		lblGrteHostadresse.setText("Gr\u00F6\u00DFte Host-Adresse:");
 		
-		txtAusgabeIP = new Text(shlNetzwerkinformationenBerechnen, SWT.READ_ONLY | SWT.MULTI);
-		txtAusgabeIP.setBounds(148, 90, 250, 21);
+		txtAusgabeIP = new Text(shlNetzwerkinformationenBerechnen,SWT.READ_ONLY | SWT.MULTI);
+		txtAusgabeIP.setBounds(174, 98, 269, 24);
 		
-		txtAusgabeNetzwerk = new Text(shlNetzwerkinformationenBerechnen, SWT.READ_ONLY | SWT.MULTI);
-		txtAusgabeNetzwerk.setBounds(148, 111, 250, 21);
+		txtAusgabeNetzwerk = new Text(shlNetzwerkinformationenBerechnen,SWT.READ_ONLY | SWT.MULTI);
+		txtAusgabeNetzwerk.setBounds(174, 125, 269, 24);
 		
-		txtAusgabeSubnetzmaske = new Text(shlNetzwerkinformationenBerechnen, SWT.READ_ONLY | SWT.MULTI);
-		txtAusgabeSubnetzmaske.setBounds(148, 132, 250, 21);
+		txtAusgabeSubnetzmaske = new Text(shlNetzwerkinformationenBerechnen,SWT.READ_ONLY | SWT.MULTI);
+		txtAusgabeSubnetzmaske.setBounds(174, 152, 269, 24);
 		
-		txtAusgabeMinHost = new Text(shlNetzwerkinformationenBerechnen, SWT.READ_ONLY | SWT.MULTI);
-		txtAusgabeMinHost.setBounds(148, 153, 250, 21);
+		txtAusgabeMinHost = new Text(shlNetzwerkinformationenBerechnen,SWT.READ_ONLY | SWT.MULTI);
+		txtAusgabeMinHost.setBounds(174, 179, 269, 24);
 		
 		txtAusgabeMaxHost = new Text(shlNetzwerkinformationenBerechnen, SWT.READ_ONLY | SWT.MULTI);
-		txtAusgabeMaxHost.setBounds(148, 174, 250, 21);
+		txtAusgabeMaxHost.setBounds(174, 206, 269, 24);
 		
 		
 
--- src/IP_Rechner/mainGui.java
+++ src/IP_Rechner/mainGui.java
@@ -45,11 +45,11 @@ public class mainGui {
 	 */
 	protected void createContents() {
 		shlIpRechner = new Shell();
-		shlIpRechner.setSize(450, 214);
+		shlIpRechner.setSize(450, 241);
 		shlIpRechner.setText("IP Rechner");
 		
 		Label lblBiteWhlenSie = new Label(shlIpRechner, SWT.NONE);
-		lblBiteWhlenSie.setBounds(10, 10, 414, 15);
+		lblBiteWhlenSie.setBounds(10, 10, 414, 21);
 		lblBiteWhlenSie.setText("W\u00E4hlen Sie eine Aktion aus:");
 		
 		Button btnNetzwerkinformationenBerechnen = new Button(shlIpRechner, SWT.NONE);
@@ -60,23 +60,23 @@ public class mainGui {
 				Infos.open();
 			}
 		});
-		btnNetzwerkinformationenBerechnen.setBounds(90, 32, 250, 25);
+		btnNetzwerkinformationenBerechnen.setBounds(90, 37, 250, 25);
 		btnNetzwerkinformationenBerechnen.setText("Netzwerkinformationen berechnen");
 		
 		Button btnVlsm = new Button(shlIpRechner, SWT.NONE);
-		btnVlsm.setBounds(90, 63, 250, 25);
+		btnVlsm.setBounds(90, 68, 250, 25);
 		btnVlsm.setText("VLSM");
 		
 		Button btnSubnetzmaskePrefix = new Button(shlIpRechner, SWT.NONE);
-		btnSubnetzmaskePrefix.setBounds(90, 94, 250, 25);
+		btnSubnetzmaskePrefix.setBounds(90, 99, 250, 25);
 		btnSubnetzmaskePrefix.setText("Subnetzmaske -> Prefix");
 		
 		Button btnAnzahlHosts = new Button(shlIpRechner, SWT.NONE);
-		btnAnzahlHosts.setBounds(90, 125, 250, 25);
+		btnAnzahlHosts.setBounds(90, 130, 250, 25);
 		btnAnzahlHosts.setText("Anzahl Hosts -> Prefix");
 		
 		Label lblChristoph = new Label(shlIpRechner, SWT.NONE);
-		lblChristoph.setBounds(10, 156, 414, 15);
+		lblChristoph.setBounds(10, 161, 414, 21);
 		lblChristoph.setText("\u00A9 2019 Christoph Pircher");
 
 	}
