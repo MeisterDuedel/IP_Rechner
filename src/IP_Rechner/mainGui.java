018392226fb5bbef5933a86df308569992907ae7
MeisterDuedel <christoph@christoph-pircher.de>
2019-11-17 16:06:42 +0100

    MainGUI, Testen auf Linux

--- src/IP_Rechner/mainGui.java
+++ src/IP_Rechner/mainGui.java
@@ -2,10 +2,14 @@ package IP_Rechner;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Button;
 
 public class mainGui {
 
-	protected Shell shell;
+	protected Shell shlIpRechner;
 
 	/**
 	 * Launch the application.
@@ -26,9 +30,9 @@ public class mainGui {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlIpRechner.open();
+		shlIpRechner.layout();
+		while (!shlIpRechner.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -39,10 +43,33 @@ public class mainGui {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+		shlIpRechner = new Shell();
+		shlIpRechner.setSize(450, 214);
+		shlIpRechner.setText("IP Rechner");
+		
+		Label lblBiteWhlenSie = new Label(shlIpRechner, SWT.NONE);
+		lblBiteWhlenSie.setBounds(10, 10, 414, 15);
+		lblBiteWhlenSie.setText("W\u00E4hlen Sie eine Aktion aus:");
+		
+		Button btnNetzwerkinformationenBerechnen = new Button(shlIpRechner, SWT.NONE);
+		btnNetzwerkinformationenBerechnen.setBounds(90, 32, 250, 25);
+		btnNetzwerkinformationenBerechnen.setText("Netzwerkinformationen berechnen");
+		
+		Button btnVlsm = new Button(shlIpRechner, SWT.NONE);
+		btnVlsm.setBounds(90, 63, 250, 25);
+		btnVlsm.setText("VLSM");
+		
+		Button btnSubnetzmaskePrefix = new Button(shlIpRechner, SWT.NONE);
+		btnSubnetzmaskePrefix.setBounds(90, 94, 250, 25);
+		btnSubnetzmaskePrefix.setText("Subnetzmaske -> Prefix");
+		
+		Button btnAnzahlHosts = new Button(shlIpRechner, SWT.NONE);
+		btnAnzahlHosts.setBounds(90, 125, 250, 25);
+		btnAnzahlHosts.setText("Anzahl Hosts -> Prefix");
+		
+		Label lblChristoph = new Label(shlIpRechner, SWT.NONE);
+		lblChristoph.setBounds(10, 156, 414, 15);
+		lblChristoph.setText("\u00A9 2019 Christoph Pircher");
 
 	}
-
 }
